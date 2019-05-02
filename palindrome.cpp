e816831cf22a3dc93a39641054bf7fedb1b0d119
SoumendraM <mookerjee.soumendra@gmail.com>
2019-05-02 08:19:00 -0700

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+void anaPalin(string s1) {
+        map<char, int> m1;
+        int odd_cnt = 0;
+
+        for (int i = 0; i < s1.size(); i++) {
+                if (m1.find(s1[i]) != m1.end()) {
+                        m1[s1[i]] += 1;
+                } else {
+                        m1[s1[i]] = 1;
+                }
+        }
+
+        for(map<char, int>::iterator it = m1.begin();
+                       it != m1.end(); it++) {
+                if ((it->second)%2 == 1) {
+                        odd_cnt += 1;
+                }
+        }
+        if ((odd_cnt == 0) || ((s1.size())%2 == 1 && odd_cnt == 1))
+                cout << "Yes\n";
+        else
+                cout << "No\n";
+}
+
+int main()
+{
+        int t;
+        cin >> t;
+        getchar();
+        while (t--) {
+                string s;
+                getline(cin, s);
+                anaPalin(s);
+        }
+}
